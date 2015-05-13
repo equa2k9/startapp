74566d5f6cee7312202b957bf1e409351cb4e96a
equa2k9 <jek3211@gmail.com>
2015-05-13 19:10:03 +0300

    fix commit

---
+++ frontend/modules/administrator/views/drivers/_driversComments.php
@@ -0,0 +1,69 @@
+<?php $this->beginWidget(
+    'booster.widgets.TbModal',
+    array('id' => 'drivers-comments')
+); ?>
+
+    <div class="modal-header">
+        <a class="close" data-dismiss="modal">&times;</a>
+        <h4>Comments</h4>
+    </div>
+
+    <div class="modal-body">
+        <p><?php
+            $this->widget('zii.widgets.CListView', array(
+                'dataProvider'=>new CActiveDataProvider('DriversComments',array('criteria'=>array('condition'=>'users_id='.$model->id),'pagination'=>false)),
+                'itemView'=>'_comment',
+                'id'=>'commentViewList',
+                'template'=>'{items}',
+                'enablePagination'=>false,
+                'enableSorting'=>false
+            ));
+            ?></p>
+        <?php
+        $comments = new DriversComments();
+
+    $form = $this->beginWidget(
+        'booster.widgets.TbActiveForm', array(
+            'id' => 'comments-form',
+            'action' => Yii::app()->createUrl('administrator/drivers/addComment'),
+            'type' => 'horizontal',
+            'enableAjaxValidation' => false,
+            'enableClientValidation' => true,
+            'htmlOptions' => array('class' => 'well', 'validateOnSubmit' => false), // for insert effect
+        )
+    );
+    ?>
+            <?php echo $form->textAreaGroup($comments,'message', array('rows'=>3)); ?>
+            <?php echo $form->hiddenField($comments,'users_id',array('value'=>$model->id))?>
+            <?php echo $form->hiddenField($comments,'leave_comment_id',array('value'=>Yii::app()->user->id))?>
+
+
+    </div>
+
+    <div class="modal-footer">
+        <?php
+        echo CHtml::ajaxSubmitButton(
+            'Save', Yii::app()->createUrl('administrator/drivers/addComment'), array(
+            'type' => 'POST',
+            'data' => 'js:$("#comments-form").serialize()',
+            'success' => 'js:function(data){
+            var obj = jQuery.parseJSON(data);
+                if(obj.status=="success")
+                {
+                    $.fn.yiiListView.update("commentViewList");
+                }
+            }'
+        ), array('class' => 'btn btn-primary',)
+        );
+        ?>
+        <?php $this->widget(
+            'booster.widgets.TbButton',
+            array(
+                'label' => 'Close',
+                'url' => '#',
+                'htmlOptions' => array('data-dismiss' => 'modal'),
+            )
+        ); ?>
+    </div>
+<?php $this->endWidget(); ?>
+<?php $this->endWidget(); ?>
